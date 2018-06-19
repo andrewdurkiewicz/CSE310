06194768bb899749a5c8cd1525cc5e0d5bfd6a50
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-06-19 15:21:50 -0700

    fix conflicts
    This reverts commit 0965f83f5c373031110fe7de7a396699fb74f89a.

--- HashTable/HashTable/HashTable/HashTable.cpp
+++ HashTable/HashTable/HashTable/HashTable.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <time.h>
 using namespace std;
 
 struct node
 {
-	node * left;
 	node * right;
 	node * head;
 	string name;
@@ -15,18 +15,33 @@ struct node
 };
 
 int getTotalStudents(int type);
-void insert(node *hash[65536], int k, string thisName, string thisLevel, double thisGPA);
+void insert(node *hash[1000], int k, string thisName, string thisLevel, double thisGPA);
 int main()
 {
-	int choice,k, levelChoice;
+	srand(time(0));
+	int choice, levelChoice;
 	string stu_name;
 	string thislevel;
 	double GPA;
 	string level[4] = { "Freshman", "Sophomore", "Junior", "Senior" };
 
-	node** hash =  new node*[65536];
+	node* hash[1000];
+	node* tmp = new node;
+	tmp->right = NULL;
+	tmp->name = "";
+	tmp->ALevel = "";
+	tmp->gpa = 0.0;
+	tmp->head = NULL;
+
+	for (int i = 0; i < 1000; i++)
+	{
+		hash[i] = tmp;
+	}
 	while (true)
 	{
+		int k = rand() % 65536;
+		int index = (k) % 1000;
+
 		cout << "Welcome to Arizona State University. There Are currently " << getTotalStudents(0) << " students in our system" << endl;
 		cout << "1. Insert a student" << endl;
 		cout << "2. Delete a student" << endl << flush;
@@ -39,12 +54,12 @@ int main()
 			cout << "What is their academic level? Select from the following:" << endl;
 			cout << "1. Freshman" << endl << "2. Sophomore" << endl << "3. Junior" << endl << "4. Senior" << endl << flush;
 			cin >> levelChoice;
-			thislevel = level[levelChoice + 1];
+			thislevel = level[levelChoice - 1];
 			cout << "What is their GPA?" << endl;
 			cin >> GPA;
-			cout << "What is the key value?" << endl;
-			cin >> k;
+			cout << "attempting to add in index: " << index << " with a key value of: " << k << endl;
 			insert(hash, k, stu_name, thislevel, GPA);
+
 		}
 	}
 	system("pause");
@@ -57,56 +72,64 @@ int getTotalStudents(int type) //type 0 is reading, type 1 is increasing, type 2
 	static int size = 0;
 	switch (type)
 	{
-	case 0 :
+	case 0:
 		break;
 	case 1:
 		size++;
+		break;
 	case 2:
 		size--;
+		break;
 	};
 	return size;
 }
 
-void insert(node* hash[65536], int k, string thisName, string thisLevel, double thisGPA)
+void insert(node* hash[1000], int k, string thisName, string thisLevel, double thisGPA)
 {
 	int size = getTotalStudents(0);
 	if (size <= 10000)
 	{
 
 		node* n = new node();
-		int key = k % 1000;
-		if (hash[key] != NULL)
+		int index = k % 1000;
+		if (hash[index] != NULL)
 		{
 			//chain
-			node* tmp = hash[key];
+			node* tmp = hash[index];
 			n->head = tmp;
 			while (tmp->right != NULL)
 			{
-				tmp = tmp->right;
+				if (tmp->right->key != k)
+				{
+					tmp = tmp->right;
+
+				}
+				else
+				{
+					cout << "Error! key already exists! Rehash necessary" << endl;
+					break;
+				}
+
+				tmp->right = n;
+				n->name = thisName;
+				n->ALevel = thisLevel;
+				n->gpa = thisGPA;
+				n->key = k;
+
 			}
-			tmp->right = n;
-			n->left = tmp;
-			n->name = thisName;
-			n->ALevel = thisLevel;
-			n->gpa = thisGPA;
-			
-		}
-		else if (hash[key] == NULL)
+		else if (hash[index] == NULL)
 		{
-			hash[key] = n;
+			hash[index] = n;
 			n->head = n;
-			n->left = NULL;
 			n->name = thisName;
 			n->ALevel = thisLevel;
 			n->gpa = thisGPA;
 			n->right = NULL;
 		}
 		getTotalStudents(1);
+		}
+		else
+		{
+			cout << "University Full" << endl;
+		}
 	}
-	else
-	{
-		cout << "University Full" << endl;
-	}
-}
-
-
