2089ca55f62b12ed517542943992dbb4d200a049
Andrew Durkiewicz <andrewdurkiewicz@gmail.com>
2018-06-19 17:00:38 -0700

    Pretty much done

--- HashTable/HashTable/HashTable/HashTable.cpp
+++ HashTable/HashTable/HashTable/HashTable.cpp
@@ -17,10 +17,11 @@ struct node
 int getTotalStudents(int type);
 void insert(node *hash[1000], int keyValue, string thisName, string thisLevel, double thisGPA);
 void printList(node*hash[1000]);
+node *Search(node* hash[1000], int findKey);
 int main()
 {
 	srand(time(0));
-	int choice, levelChoice;
+	int choice, levelChoice, y;
 	string stu_name, last_Name, thislevel;
 	double GPA;
 	string level[4] = { "Freshman", "Sophomore", "Junior", "Senior" };
@@ -31,38 +32,64 @@ int main()
 	{
 		hash[i] = new node;
 	}
+	cout << endl << "Welcome to Arizona State University." << endl << endl;
 	while (true)
 	{
 		int keyValue = rand() % 65536;
 		int index = keyValue % 1000;
 
-		cout << "Welcome to Arizona State University. There Are currently " << getTotalStudents(0) << " students in our system" << endl;
+		cout << "# of Students in Database: "<< getTotalStudents(0) << endl;
+		cout << "Select From the Menu Below:" << endl;
+		cout << "------------------------------" << endl;
 		cout << "1. Insert a student" << endl;
 		cout << "2. Delete a student" << endl << flush;
 		cout << "3. Print current list" << endl;
+		cout << "4. Search the table" << endl;
+		cout << "9. Exit" << endl;
 		cin >> choice;
 		switch (choice)
 		{
 		case 1:
-			cout << "Enter first name?" << endl << flush;
+			cout << endl << "Enter first name: " << endl << flush;
 			cin >> stu_name;
-			cout << "Enter Last name?" << endl << flush;
+			cout << endl << "Enter Last name: " << endl << flush;
 			cin >> last_Name;
 			stu_name = stu_name + " " + last_Name;
-			cout << "What is their academic level? Select from the following:" << endl;
+			cout << endl << "What is their academic level? Select from the following:" << endl;
 			cout << "1. Freshman" << endl << "2. Sophomore" << endl << "3. Junior" << endl << "4. Senior" << endl << flush;
 			cin >> levelChoice;
 			thislevel = level[levelChoice - 1];
-			cout << "What is their GPA?" << endl;
+			cout << endl << "What is their GPA?" << endl;
 			cin >> GPA;
-			cout << "attempting to add in index: " << index << endl;
+			//cout << "attempting to add in index: " << index << endl;
+			//cout << "Key Level: " << keyValue;
 			insert(hash, keyValue, stu_name, thislevel, GPA);
+			cout << endl << "Insert Completed" << endl << endl;
 		case 2:
 			break;
 		case 3:
 			printList(hash);
 			break;
-
+		case 4:
+			cout << "Enter a key value (between 0-65536) to search for: " << endl << flush;
+			cin >> y;
+			while (y < 0 && y> 65536)
+			{
+				cout << "Error, key must be between 0 and 65536, please retry." << endl << flush;
+				cin >> y;
+			}
+			node* fromsearch = Search(hash, y);
+			if (fromsearch == NULL)
+			{
+				cout << "Nothing was found with a key value of: " << y;
+			}
+			else
+			{
+				cout << "The following student profile was located with a key value of: " << endl << y;
+				cout << "Name: " << fromsearch->name << endl;
+				cout << "GPA: " << fromsearch->gpa << endl;
+				cout << "Academic Level" << fromsearch->ALevel << endl;
+			}
 		}
 	}
 	system("pause");
@@ -160,4 +187,24 @@ void printList(node * hash[1000])
 		}
 	}
 	cout << endl << endl;
+}
+
+node* Search(node * hash[1000], int findKey)
+{
+	node* tmp;
+	int k = findKey % 1000;
+	tmp = hash[k]->head;
+	if (tmp == NULL)
+	{
+		return NULL;
+	}
+	while (tmp->right != NULL);
+	{
+		if (tmp->key == findKey)
+		{
+			return tmp;
+		}
+		tmp = tmp->right;
+	}
+	return NULL;
 }
